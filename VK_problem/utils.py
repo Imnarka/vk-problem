4527b9f59cd22c3611872ea3fac76555b1209783
Linar Fazlykhakov <67334757+Imnarka@users.noreply.github.com>
2022-02-17 19:50:00 +0500

    Delete VK_problem directory

--- VK_problem/utils.py
+++
@@ -1,9 +0,0 @@
-from math import floor, sqrt
-
-def is_prime(x):
-    if x < 2:
-        return False
-    for i in range(2, floor(sqrt(x) + 1)):
-        if x % i == 0:
-            return False
-    return True
